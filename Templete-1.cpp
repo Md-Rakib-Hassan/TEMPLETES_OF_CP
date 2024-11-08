bdc20572ce205a63f163d50c0d73f6b2a3166730
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-11-08 10:05:31 +0600

    initial commit

---
+++ Templete-1.cpp
@@ -0,0 +1,46 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include<bits/stdc++.h>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define rep(i,x,n) for(int i=x;i<n;i++)
+#define repv(i,n,x) for(int i=n;i>x;i--)
+#define test int tt; cin>>tt; for(int tc=1;tc<=tt;tc++)
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);clock_t tStart = clock()
+#define e_time cout<<"Time : "<<fixed<<setprecision(10)<<(double)((clock()-tStart))/CLOCKS_PER_SEC<<" S"<<endl
+#define all(we) we.begin(),we.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n" 
+#define vin(v,ll,n) int n;cin>>n;vector<ll>v;rep(i,0,n){ll x; cin>>x;v.pb(x);}
+#define vinn(v,ll,n)vector<ll>v;rep(i,0,n){ll x; cin>>x;v.pb(x);}
+#define vin_ex(v,ll,n) int n;cin>>n;vector<ll>v;rep(i,0,n){ll x; cin>>x;v.pb(x);
+#define coutcase cout<<"Case "<<tc<<": " 
+//const int sz=1e5+10;
+
+
+
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>ordered_set;
+int main()
+{
+    fast;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+
+
+
+
+
+
+
+
+#ifndef ONLINE_JUDGE
+    e_time;
+#endif
+return 0;
+}
---
+++ Templete-2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(we) we.begin(),we.end()
+#define pb push_back
+#define ff first
+#define ss second
+#define nl "\n" 
+
+
+int main()
+{
+
+
+
+return 0;
+}
